76c1f5c394d6c0964c1ff40611f4880f57da84d9
Hamza Bakh <117382056+Hamzabakh1@users.noreply.github.com>
2025-05-26 10:37:48 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Heroku dyno port
-if [ "$PORT" ]; then
+# Bind Heroku port to Metabase
+if [ -n "${PORT:-}" ]; then
   export MB_JETTY_PORT="$PORT"
 fi
 
 # Use Heroku DATABASE_URL
-if [ "$DATABASE_URL" ]; then
+if [ -n "${DATABASE_URL:-}" ]; then
   export MB_DB_CONNECTION_URI="$DATABASE_URL"
 fi
 
-# Java memory flags for container
-JAVA_OPTS="\$JAVA_OPTS -XX:+UnlockExperimentalVMOptions"
-JAVA_OPTS="\$JAVA_OPTS -XX:+UseContainerSupport"
-JAVA_OPTS="\$JAVA_OPTS -XX:-UseGCOverheadLimit"
-JAVA_OPTS="\$JAVA_OPTS -XX:+UseG1GC"
-JAVA_OPTS="\$JAVA_OPTS -XX:+UseStringDeduplication"
-JAVA_OPTS="\$JAVA_OPTS -server"
+# Build Java options
+JAVA_OPTS="${JAVA_TOOL_OPTIONS:-}"
+JAVA_OPTS="$JAVA_OPTS -XX:+UnlockExperimentalVMOptions"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseContainerSupport"
+JAVA_OPTS="$JAVA_OPTS -XX:-UseGCOverheadLimit"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseG1GC"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseStringDeduplication"
+JAVA_OPTS="$JAVA_OPTS -server"
 
 # Execute Metabase jar
-exec java \$JAVA_OPTS -jar /app/metabase.jar
+exec java $JAVA_OPTS -jar /app/metabase.jar
