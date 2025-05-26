adcea72449de8ba9c121e73b829d79a0bc360a00
Hamza Bakh <117382056+Hamzabakh1@users.noreply.github.com>
2025-05-26 10:38:06 +0100

    Update build-driver-cache.sh

--- bin/build-driver-cache.sh
+++ bin/build-driver-cache.sh
@@ -2,11 +2,9 @@
 set -euo pipefail
 
 # Define Snowflake JDBC version
-SNOWFLAKE_JDBC_VERSION="3.13.23"
-
-# Download from Maven Central
+SNOWFLAKE_JDBC_VERSION="3.23.0"
 JAR_URL="https://repo1.maven.org/maven2/net/snowflake/snowflake-jdbc/${SNOWFLAKE_JDBC_VERSION}/snowflake-jdbc-${SNOWFLAKE_JDBC_VERSION}.jar"
 
 # Prepare plugins directory and fetch driver
 mkdir -p /app/plugins
-curl -L "${JAR_URL}" -o /app/plugins/snowflake-jdbc.jar
+curl -fsSL "$JAR_URL" -o /app/plugins/snowflake-jdbc.jar
